991e1fa0211993af0d1b08699e8a0105ee713402
mounir <165167690+mhaddou1337@users.noreply.github.com>
2025-02-10 11:55:06 +0100

    Add files via upload

---
+++ ft_abs/ft_abs.c
@@ -0,0 +1,16 @@
+int ft_abs(int x)
+{
+    if (x < 0)
+        x = -x;
+    return (x);
+}
+/*
+#include <stdio.h>
+int main(void)
+{
+    printf("%d\n", ft_abs(-72));
+    printf("%d\n", ft_abs(+72));
+    printf("%d\n", ft_abs(72));
+    return(0);
+}
+*/
---
+++ ft_abs/ft_abs.py
@@ -0,0 +1,9 @@
+def ft_abs(x):
+    if x < 0:
+        x = -x
+    return x
+"""  
+print(ft_abs(-2))
+print(ft_abs(+2))
+print(ft_abs(2))
+"""
