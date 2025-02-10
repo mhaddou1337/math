cc72045128f9090bcf517ba0a1a4d95a64d5d2cc
mounir <165167690+mhaddou1337@users.noreply.github.com>
2025-02-10 11:50:38 +0100

    Update eucldian_algo.c

--- eucldian_algo/eucldian_algo.c
+++ eucldian_algo/eucldian_algo.c
@@ -1,5 +1,3 @@
-#include <unistd.h>
-#include <stdio.h>
 int eucl_algo(int a , int b)
 {
     while(b != 0)
@@ -10,8 +8,12 @@ int eucl_algo(int a , int b)
     }
     return(a);
 }
+/*
+#include <unistd.h>
+#include <stdio.h>
 int main(void)
 {
     printf("%d", eucl_algo(72,48));
     return(0);
 }
+*/
