e19f606baa2c3f872c07099eda3973a9d0765691
mounir <165167690+mhaddou1337@users.noreply.github.com>
2025-02-10 11:48:57 +0100

    Update and rename v_abs.py to ft_fabs.py

---
+++ v_abs/ft_fabs.py
@@ -0,0 +1,9 @@
+def ft_fabs(nb):
+    if nb < 0:
+        nb = -nb
+    return nb
+"""
+print(ft_fabs(-2.94847))
+print(ft_fabs(2.94847))
+print(ft_fabs(+2.94847))
+"""
--- v_abs/v_abs.py
+++
@@ -1,8 +0,0 @@
-def v_abs(nb):
-    if nb < 0:
-        nb = -nb
-    return nb
-
-print(v_abs(-2))
-print(v_abs(2))
-print(v_abs(+2))
